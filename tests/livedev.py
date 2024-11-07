63e7296394af50b2294f66827e4bc4bf32b4d20b
Ben Kamphaus <ben.kamphaus@gmail.com>
2024-11-07 07:27:53 -0700

    drop measurement generator now that it's in dataset

--- tests/livedev.py
+++ tests/livedev.py
@@ -31,39 +31,8 @@ sites
 resp = pqq.datoms(":aevt", [":measurement/rsem-normalized-count"], db_name='tcga-brca')
 sample_chunk = resp["datoms_chunk"]
 
-def measurement_generator(meas_attr=":measurement/rsem-normalized-count", db_name='tcga-brca',
-                          chunk_size=5000):
-    offset = 0
-    api_resp = pqq.datoms(":aevt", [meas_attr], db_name=db_name,
-                          offset=offset, limit=chunk_size)
-    chunk = api_resp["datoms_chunk"]
-    while len(chunk) >= 1:
-        meas_eids = [elem[":e"] for elem in chunk]
-        meas_res = pqq.query({
-            ":find": ["?m", "?hgnc", "?samp-id"],
-            ":in": ["$", ["?m", "..."]],
-            ":where":
-                [["?m", ":measurement/gene-product", "?gp"],
-                 ["?gp", ":gene-product/gene", "?g"],
-                 ["?g", ":gene/hgnc-symbol", "?hgnc"],
-                 ["?m", ":measurement/sample", "?s"],
-                 ["?s", ":sample/id", "?samp-id"]]
-            },
-            args=[meas_eids],
-            db_name='tcga-brca'
-        )
-        lookup = dict([(meas[0], (meas[1], meas[2])) for meas in meas_res["query_result"]])
-        for datom in chunk:
-            eid = datom[":e"]
-            match = lookup[eid]
-            yield eid, match[0], match[1], datom[":v"]
-        offset += chunk_size
-        api_resp = pqq.datoms(":aevt", [meas_attr], db_name=db_name,
-                              offset=offset, limit=chunk_size)
-        chunk = api_resp["datoms_chunk"]
-    raise StopIteration
 
-meas_gen = measurement_generator()
+meas_gen = pqd.measurement_generator()
 next(meas_gen)
 
 
